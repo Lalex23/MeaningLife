bdd6a502e5d6b7bd0b68e6646d7b930d00c218a8
Лынник Александр <sanya.lynnik.84@mail.ru>
2023-02-03 03:49:01 +0600

    Попытка сделать анализ кода

--- utils.py
+++ utils.py
@@ -16,23 +16,57 @@ class DisplayResultImage:
         cv2.destroyAllWindows()
 
 
+class TextAnalysis:
+    def __init__(self, text):
+        self.text = text
+        self.code = ""
+        self.file_name = None
+
+    def make(self):
+        for count, line in enumerate(self.text.split('\n'), start=1):
+            new_line = " ".join(line.split(' ')[1::])
+            print(repr(new_line))
+            if new_line.startswith('import'):
+                self.code += new_line + "\n\r"
+            elif new_line.startswith(' public class') and new_line.endswith('{'):
+                self.code += "\r" + new_line.strip() + "\n\t"
+            elif new_line.startswith('private final'):
+                self.code += "\t" + new_line.strip() + "\n"
+            if count == 10:
+                break
+
+        print(self.code)
+
+    def get_code(self):
+        return self.code
+
+
 class ExtractJavaCodeFromImage(object):
     PATH_TO_IMAGE = "java-code.png"
 
     def __init__(self):
         super().__init__()
-
+        # Укажи путь до исполняемого файла tesseract
         pytesseract.pytesseract.tesseract_cmd = r'E:\Program Files\Tesseract-OCR\tesseract.exe'
 
     def get_code(self):
-        prepared_image = self.prepare_image_erode_dilate()
+        prepared_image = self.prepare_image_filter_a()
 
-        text = pytesseract.image_to_string(prepared_image, lang='eng', config='--psm 6 --oem 2')
+        DisplayResultImage(prepared_image).show()
 
-        for string in text.split("\n"):
-            print(string)
+        custom_configure = '-l eng --psm 6 --oem 2'
+        text = pytesseract.image_to_string(prepared_image, config=custom_configure)
 
-    def prepare_image_hsv_type(self):
+        # for string in text.split("\n"):
+        #     print(string)
+
+        text_analysis = TextAnalysis(text)
+        text_analysis.make()
+        code = text_analysis.get_code()
+
+        #print(code)
+
+    def prepare_image_filter_b(self):
         image = cv2.imread(self.PATH_TO_IMAGE)
 
         (H, W) = image.shape[:2]
@@ -54,22 +88,19 @@ class ExtractJavaCodeFromImage(object):
 
         return image_result
 
-    def prepare_image_erode_dilate(self):
+    def prepare_image_filter_a(self):
         # read image with openCv
         image_original = cv2.imread(self.PATH_TO_IMAGE)
+
         # Convert to GrayScale
         image_gray = cv2.cvtColor(image_original, cv2.COLOR_BGR2GRAY)
 
         # Apply dilation and erosion to remove some noise
-        kernel = np.ones((1, 1), np.uint8)
+        kernel = np.ones((15, 15), np.uint8)
         image_dilate = cv2.dilate(image_gray, kernel, iterations=1)
         image_erode = cv2.erode(image_dilate, kernel, iterations=1)
 
-        DisplayResultImage(image_dilate).show()
-        DisplayResultImage(image_erode).show()
+        image_result = 250 - cv2.bitwise_and(image_gray, image_gray, mask=image_erode)
 
-        image_result = cv2.bitwise_and(image_erode, image_erode)
-
-        # Apply threshold to get image with only black and white
-        #image_result = cv2.adaptiveThreshold(image_erode, 10, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
         return image_result
+

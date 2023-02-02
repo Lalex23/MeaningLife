25c80f4701f4372098bd6468b52c93c22d216db1
Лынник Александр <sanya.lynnik.84@mail.ru>
2023-02-03 01:33:15 +0600

    Попытка подобрать фильтрф для извлечения текста день второй

--- utils.py
+++ utils.py
@@ -3,44 +3,46 @@ import numpy as np
 import pytesseract
 
 
+class DisplayResultImage:
+    def __init__(self, image):
+        cv2.imshow('Image', image)
+
+    def show(self):
+        while True:
+            if cv2.waitKey(0): # close window when a key press is detected
+                break
+
+    def __del__(self):
+        cv2.destroyAllWindows()
+
+
 class ExtractJavaCodeFromImage(object):
+    PATH_TO_IMAGE = "java-code.png"
+
     def __init__(self):
         super().__init__()
 
         pytesseract.pytesseract.tesseract_cmd = r'E:\Program Files\Tesseract-OCR\tesseract.exe'
 
     def get_code(self):
-        image = cv2.imread("java-code.png")
+        prepared_image = self.prepare_image_erode_dilate()
 
-        # Конвертация в серый
-        image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        text = pytesseract.image_to_string(prepared_image, lang='eng', config='--psm 6 --oem 2')
 
-        # text = pytesseract.image_to_string(Image.open("java-code.png"), lang='eng',
-                                           # config='--psm 1 --oem 1')
+        for string in text.split("\n"):
+            print(string)
+
+    def prepare_image_hsv_type(self):
+        image = cv2.imread(self.PATH_TO_IMAGE)
 
         (H, W) = image.shape[:2]
         image_big = cv2.resize(image, (H*2, W*2))
-        # text = pytesseract.image_to_string(image_gray, lang='eng', config='--psm 6 --oem 1')
-        image_blur = cv2.bilateralFilter(image_gray, 1, 125, 390)  # Blur to reduce noise
-
-
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 5))
-
-        hsv = cv2.cvtColor(image_big, cv2.COLOR_BGR2HSV)
-        lower = np.array([0, 0, 218])
-        upper = np.array([157, 54, 255])
-        mask = cv2.inRange(hsv, lower, upper)
-        dilate = cv2.dilate(mask, kernel, iterations=5)
-        return_result, image_new = cv2.threshold(image_gray, 138, 355, cv2.THRESH_BINARY)
-
-        # cv2.imshow('image_blur', image_blur)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
+        (HB, WB) = image_big.shape[:2]
 
         # (hMin = 0, sMin = 0, vMin = 81), (hMax = 179, sMax = 255, vMax = 255)
 
         hsv = cv2.cvtColor(image_big, cv2.COLOR_BGR2HSV)
-        lower = np.array([0, 0, 81])
+        lower = np.array([0, 0, 91])
         upper = np.array([255, 255, 255])
         mask = cv2.inRange(hsv, lower, upper)
 
@@ -48,14 +50,26 @@ class ExtractJavaCodeFromImage(object):
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 3))
         dilate = cv2.dilate(mask, kernel, iterations=5)
 
-        result = 255 - cv2.bitwise_and(dilate, mask)
+        image_result = 255 - cv2.bitwise_and(dilate, mask)
 
-        cv2.imshow('image_new', result)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        return image_result
 
-        text = pytesseract.image_to_string(result, lang='eng', config='--psm 6 --oem 1')
+    def prepare_image_erode_dilate(self):
+        # read image with openCv
+        image_original = cv2.imread(self.PATH_TO_IMAGE)
+        # Convert to GrayScale
+        image_gray = cv2.cvtColor(image_original, cv2.COLOR_BGR2GRAY)
 
-        for string in text.split("\n"):
-            print(string)
+        # Apply dilation and erosion to remove some noise
+        kernel = np.ones((1, 1), np.uint8)
+        image_dilate = cv2.dilate(image_gray, kernel, iterations=1)
+        image_erode = cv2.erode(image_dilate, kernel, iterations=1)
 
+        DisplayResultImage(image_dilate).show()
+        DisplayResultImage(image_erode).show()
+
+        image_result = cv2.bitwise_and(image_erode, image_erode)
+
+        # Apply threshold to get image with only black and white
+        #image_result = cv2.adaptiveThreshold(image_erode, 10, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+        return image_result
